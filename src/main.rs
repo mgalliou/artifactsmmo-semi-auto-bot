401111979674f0f72f0f471c0ea6e6b7d0c7090d
mgalliou <matthias.galliou@gmail.com>
2024-08-29 21:26:28 +0200

    feat: improve main

--- src/main.rs
+++ src/main.rs
@@ -8,68 +8,69 @@ use std::{
     thread,
 };
 
-fn run() {
+fn run() -> Option<()> {
     let base_url = "https://api.artifactsmmo.com";
     let token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6InBvZEppbyIsInBhc3N3b3JkX2NoYW5nZWQiOiIifQ.Qy1Hm2-QYm84O_9aLP076TczjYDCpSuZ75dKkh9toUY";
     let account = Account::new(base_url, token);
     let bank = Arc::new(RwLock::new(Bank::new(&account)));
     let mut char1 = Character::new(
         &account,
-        &account.get_character_by_name("Jio").unwrap().name,
+        &account.get_character_by_name("Jio")?.name,
         bank.clone(),
     );
     let mut char2 = Character::new(
         &account,
-        &account.get_character_by_name("Eraly").unwrap().name,
+        &account.get_character_by_name("Eraly")?.name,
         bank.clone(),
     );
     let mut char3 = Character::new(
         &account,
-        &account.get_character_by_name("Nalgisk").unwrap().name,
+        &account.get_character_by_name("Nalgisk")?.name,
         bank.clone(),
     );
     let mut char4 = Character::new(
         &account,
-        &account.get_character_by_name("Tieleja").unwrap().name,
+        &account.get_character_by_name("Tieleja")?.name,
         bank.clone(),
     );
     let mut char5 = Character::new(
         &account,
-        &account.get_character_by_name("Kvarask").unwrap().name,
+        &account.get_character_by_name("Kvarask")?.name,
         bank.clone(),
     );
 
     let t1 = thread::Builder::new()
         .name(char1.name.to_string())
         .spawn(move || {
-            char1.run(Role::Woodcutter);
+            char1.run(Role::Idle);
         })
         .unwrap();
     let t2 = thread::Builder::new()
         .name(char2.name.to_string())
         .spawn(move || {
-            char2.run(Role::Miner);
+            char2.run(Role::Woodcutter);
         })
         .unwrap();
     let t3 = thread::Builder::new()
         .name(char3.name.to_string())
-        .spawn(move || char3.run(Role::Woodcutter))
+        .spawn(move || char3.run(Role::Miner))
         .unwrap();
     let t4 = thread::Builder::new()
         .name(char4.name.to_string())
-        .spawn(move || char4.run(Role::Miner))
+        .spawn(move || char4.run(Role::Woodcutter))
         .unwrap();
     let t5 = thread::Builder::new()
         .name(char5.name.to_string())
-        .spawn(move || char5.run(Role::Miner))
+        .spawn(move || char5.run(Role::Woodcutter))
         .unwrap();
     t1.join().unwrap();
     t2.join().unwrap();
     t3.join().unwrap();
     t4.join().unwrap();
     t5.join().unwrap();
+    Some(())
 }
 
 fn main() {
-    run()
+    let _ = run().is_some();
 }
