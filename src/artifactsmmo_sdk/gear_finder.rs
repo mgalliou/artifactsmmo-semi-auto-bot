36f45aac2a1a67aec4fdcba5f24b13e929669b45
mgalliou <matthias.galliou@gmail.com>
2024-11-14 18:04:04 +0100

    chore: remove old gear finder implementation

--- src/artifactsmmo_sdk/gear_finder.rs
+++ src/artifactsmmo_sdk/gear_finder.rs
@@ -1,6 +1,6 @@
 use super::{
     character::Character,
-    gear::{Gear, Slot},
+    gear::Gear,
     items::{Items, Type},
     ItemSchemaExt,
 };
@@ -58,8 +58,6 @@ impl GearFinder {
         filter: Filter,
         weapon: &'a ItemSchema,
     ) -> Vec<Gear> {
-        // TODO: low level gear with empty slots need to be handled properly,
-        // Maybe with `Option`s.
         let helmets =
             self.best_armors_against_with_weapon(char, monster, filter, weapon, Type::Helmet);
         let shields =
@@ -185,172 +183,6 @@ impl GearFinder {
             upgrades.push(best_for_health);
         }
         upgrades
-    }
-
-    pub fn best_available_against<'a>(
-        &'a self,
-        char: &'a Character,
-        monster: &MonsterSchema,
-    ) -> Gear {
-        let gear = char
-            .available_equipable_weapons()
-            .iter()
-            .map(|w| self.best_available_against_with_weapon(char, monster, w))
-            .max_by_key(|e| OrderedFloat(e.attack_damage_against(monster)));
-        if let Some(gear) = gear {
-            return gear;
-        }
-        char.gear()
-    }
-
-    fn best_available_against_with_weapon<'a>(
-        &'a self,
-        char: &Character,
-        monster: &MonsterSchema,
-        weapon: &'a ItemSchema,
-    ) -> Gear {
-        Gear {
-            weapon: Some(weapon),
-            shield: self.best_in_slot_available_against_with_weapon(
-                char,
-                Slot::Shield,
-                monster,
-                weapon,
-            ),
-            helmet: self.best_in_slot_available_against_with_weapon(
-                char,
-                Slot::Helmet,
-                monster,
-                weapon,
-            ),
-            body_armor: self.best_in_slot_available_against_with_weapon(
-                char,
-                Slot::BodyArmor,
-                monster,
-                weapon,
-            ),
-            leg_armor: self.best_in_slot_available_against_with_weapon(
-                char,
-                Slot::LegArmor,
-                monster,
-                weapon,
-            ),
-            boots: self.best_in_slot_available_against_with_weapon(
-                char,
-                Slot::Boots,
-                monster,
-                weapon,
-            ),
-            ring1: self.best_in_slot_available_against_with_weapon(
-                char,
-                Slot::Ring1,
-                monster,
-                weapon,
-            ),
-            ring2: self.best_in_slot_available_against_with_weapon(
-                char,
-                Slot::Ring2,
-                monster,
-                weapon,
-            ),
-            amulet: self.best_in_slot_available_against_with_weapon(
-                char,
-                Slot::Amulet,
-                monster,
-                weapon,
-            ),
-            artifact1: self.best_in_slot_available_against_with_weapon(
-                char,
-                Slot::Artifact1,
-                monster,
-                weapon,
-            ),
-            artifact2: self.best_in_slot_available_against_with_weapon(
-                char,
-                Slot::Artifact2,
-                monster,
-                weapon,
-            ),
-            artifact3: self.best_in_slot_available_against_with_weapon(
-                char,
-                Slot::Artifact3,
-                monster,
-                weapon,
-            ),
-            utility1: self.best_in_slot_available_against_with_weapon(
-                char,
-                Slot::Utility1,
-                monster,
-                weapon,
-            ),
-            utility2: self.best_in_slot_available_against_with_weapon(
-                char,
-                Slot::Utility2,
-                monster,
-                weapon,
-            ),
-        }
-    }
-
-    /// Returns the best item available for the given `slot` against the given
-    /// `monster`, based on item attack damage, damage increase and `monster`
-    /// resistances.
-    fn best_in_slot_available_against_with_weapon(
-        &self,
-        char: &Character,
-        slot: Slot,
-        monster: &MonsterSchema,
-        weapon: &ItemSchema,
-    ) -> Option<&ItemSchema> {
-        match slot {
-            Slot::Amulet if char.level() >= 5 && char.level() < 10 => self.items.get("life_amulet"),
-            Slot::BodyArmor
-            | Slot::LegArmor
-            | Slot::Helmet
-            | Slot::Ring1
-            | Slot::Ring2
-            | Slot::Amulet
-            | Slot::Boots
-            | Slot::Shield => {
-                self.best_available_armor_against_with_weapon(char, slot, monster, weapon)
-            }
-            _ => None,
-        }
-    }
-
-    /// Returns the best upgrade available in bank or inventory for the given
-    /// armor `slot` against the given `monster`, based on the currently equiped
-    /// weapon and the `monster` resitances.
-    fn best_available_armor_against_with_weapon(
-        &self,
-        char: &Character,
-        slot: Slot,
-        monster: &MonsterSchema,
-        weapon: &ItemSchema,
-    ) -> Option<&ItemSchema> {
-        let available = self
-            .items
-            .equipable_at_level(char.level(), Type::from(slot))
-            .into_iter()
-            .filter(|i| {
-                char.has_available(&i.code) > {
-                    if slot.is_ring_2() {
-                        1
-                    } else {
-                        0
-                    }
-                }
-            })
-            .collect_vec();
-        let mut upgrade = available
-            .iter()
-            .max_by_key(|i| OrderedFloat(i.damage_increase_against_with(monster, weapon)));
-        if upgrade.is_some_and(|i| i.total_damage_increase() <= 0) {
-            upgrade = available
-                .iter()
-                .min_by_key(|i| OrderedFloat(i.damage_from(monster)))
-        }
-        upgrade.copied()
     }
 
     fn is_eligible(i: &ItemSchema, filter: Filter, char: &Character) -> bool {
