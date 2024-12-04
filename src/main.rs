a924259bbe9044cbe8d7b8d82693856a0de69ffe
mgalliou <matthias.galliou@gmail.com>
2024-12-04 17:49:10 +0100

    feat(main): add malefic_cloth to global order

--- src/main.rs
+++ src/main.rs
@@ -15,11 +15,13 @@ fn main() -> rustyline::Result<()> {
     let _ = simple_logging::log_to_file("artifactsmmo.log", LevelFilter::Info);
     let game = Game::new();
     game.init();
-    game.orderboard.add(None, "snowman_hat", 10, Purpose::Cli);
-    game.orderboard.add(None, "lizard_skin", 1000, Purpose::Cli);
-    game.orderboard.add(None, "demon_horn", 1000, Purpose::Cli);
-    game.orderboard.add(None, "diamond", 200, Purpose::Cli);
-    game.orderboard.add(None, "magic_sap", 200, Purpose::Cli);
+    let _ = game.orderboard.add(None, "snowman_hat", 10, Purpose::Cli);
+    let _ = game.orderboard.add(None, "lizard_skin", 1000, Purpose::Cli);
+    let _ = game.orderboard.add(None, "demon_horn", 1000, Purpose::Cli);
+    let _ = game.orderboard.add(None, "malefic_cloth", 200, Purpose::Cli);
+    let _ = game.orderboard.add(None, "strange_ore", 6000, Purpose::Cli);
+    let _ = game.orderboard.add(None, "magic_wood", 6000, Purpose::Cli);
+    //let _ = game.orderboard.add(None, "frozen_pickaxe", 5, Purpose::Cli);
     let handles = game
         .account
         .characters
