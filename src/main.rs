08548c9902ee3f2a37f60228e60e425e268a3af9
mgalliou <matthias.galliou@gmail.com>
2024-12-16 13:38:25 +0100

    feat(main): remove carrot from default orders and use early return on them

--- src/main.rs
+++ src/main.rs
@@ -19,15 +19,18 @@ fn main() -> Result<()> {
     let _ = simple_logging::log_to_file("artifactsmmo.log", LevelFilter::Info);
     let game = Game::new();
     game.init();
-    let _ = game.orderboard.add(None, "carrot", 1000, Purpose::Cli);
-    let _ = game.orderboard.add(None, "lizard_skin", 1000, Purpose::Cli);
-    let _ = game.orderboard.add(None, "demon_horn", 1000, Purpose::Cli);
-    let _ = game
-        .orderboard
-        .add(None, "malefic_cloth", 200, Purpose::Cli);
-    let _ = game.orderboard.add(None, "strange_ore", 6000, Purpose::Cli);
-    let _ = game.orderboard.add(None, "magic_wood", 6000, Purpose::Cli);
-    //let _ = game.orderboard.add(None, "frozen_pickaxe", 5, Purpose::Cli);
+    //let _ = game.orderboard.add(None, "carrot", 1000, Purpose::Cli);
+    game.orderboard
+        .add(None, "lizard_skin", 1000, Purpose::Cli)?;
+    game.orderboard
+        .add(None, "demon_horn", 1000, Purpose::Cli)?;
+    game.orderboard
+        .add(None, "malefic_cloth", 200, Purpose::Cli)?;
+    game.orderboard
+        .add(None, "strange_ore", 6000, Purpose::Cli)?;
+    game.orderboard
+        .add(None, "magic_wood", 6000, Purpose::Cli)?;
+    //game.orderboard.add(None, "frozen_pickaxe", 5, Purpose::Cli)?;
     let handles = game
         .account
         .characters
