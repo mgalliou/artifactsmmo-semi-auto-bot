10464c18116663d2e6f0c13afda857062dec444b
mgalliou <matthias.galliou@gmail.com>
2025-01-24 16:11:39 +0100

    fix(main): update ORDER_BOARD imports

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,20 @@
 use anyhow::Result;
-use artifactsmmo_sdk::{GAME, orderboard::Purpose};
+use artifactsmmo_sdk::{
+    orderboard::{Purpose, ORDER_BOARD},
+    GAME,
+};
 use log::LevelFilter;
 
 fn main() -> Result<()> {
     simple_logging::log_to_file("artifactsmmo.log", LevelFilter::Info)?;
-    GAME.init();
     //game.orderboard
     //    .add(None, "lizard_skin", 1000, Purpose::Cli)?;
     //game.orderboard
     //    .add(None, "demon_horn", 1000, Purpose::Cli)?;
-    GAME.orderboard
-        .add(None, "malefic_cloth", 200, Purpose::Cli)?;
-    GAME.orderboard
-        .add(None, "rosenblood_elixir", 200, Purpose::Cli)?;
-    GAME.orderboard
-        .add(None, "strange_ore", 6000, Purpose::Cli)?;
-    GAME.orderboard
-        .add(None, "magic_wood", 6000, Purpose::Cli)?;
+    ORDER_BOARD.add(None, "malefic_cloth", 200, Purpose::Cli)?;
+    ORDER_BOARD.add(None, "rosenblood_elixir", 200, Purpose::Cli)?;
+    ORDER_BOARD.add(None, "strange_ore", 6000, Purpose::Cli)?;
+    ORDER_BOARD.add(None, "magic_wood", 6000, Purpose::Cli)?;
     //game.orderboard.add(None, "carrot", 1000, Purpose::Cli);
     //game.orderboard.add(None, "frozen_pickaxe", 5, Purpose::Cli)?;
     GAME.run_characters();
