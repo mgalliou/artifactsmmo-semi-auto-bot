c30c4a070a737dda8146559b6c16663ab5081f3d
mgalliou <matthias.galliou@gmail.com>
2026-02-28 19:14:07 +0100

    feat(openapi): update generation config and ignored files

--- crates/openapi/gen.sh
+++ crates/openapi/gen.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 declare -A PROPERTIES
-PROPERTIES[packageName]="artifactsmmo-openapi"
+PROPERTIES[packageName]="openapi"
 PROPERTIES[supportAsync]="false"
 PROPERTIES[preferUnsignedInt]="true"
 
@@ -11,7 +11,8 @@ for key in "${!PROPERTIES[@]}"; do
 done
 
 rm -rf src/ docs/ Cargo.toml git_push.sh openapitools.json README.md
-npx @openapitools/openapi-generator-cli generate -i openapi.json -g rust -o . --additional-properties="$ADDITIONAL_PROPERTIES"
+npx @openapitools/openapi-generator-cli generate -i openapi.json -g rust -o . -c ./generator-config.yaml
+#--additional-properties="$ADDITIONAL_PROPERTIES"
 
 # fix errors in generation
 find . -type f -name "*.rs" -exec sed -i 's/models::models::/models::/g' {} +
